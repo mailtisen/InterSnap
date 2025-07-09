57bba6746b7089b24ecce4f4343f0dedc995c47c
mailtisen <you@example.com>
2025-07-10 00:00:09 +0530

    updated

--- src/invoke snapshot/invoke.sh
+++ src/invoke snapshot/invoke.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 export PATH=$PATH:/usr/local/go/bin
 export PATH=${PWD}/../../bin:$PATH
 export FABRIC_CFG_PATH=${PWD}/../../config/
@@ -8,11 +8,29 @@ export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/../organizations/peerOrganizations/org
 export CORE_PEER_MSPCONFIGPATH=${PWD}/../organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 export CORE_PEER_ADDRESS=localhost:7051
 d="$(date +"%d-%m-%Y")"
-#go run /home/workspace/snapshotwork_code/copySnapshotFromPeer.go
-peer channel getinfo -c mychannel
-peer snapshot submitrequest -c mychannel -b 0 --peerAddress localhost:7051 --tlsRootCertFile ${PWD}/../organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-docker cp peer0.org1.example.com:/var/hyperledger/production/snapshots/completed/mychannel/ ${PWD}/archive/$d
-cp ${PWD}/archive/txnlist.dat ${PWD}/archive/$d
-touch  ${PWD}/archive/filead120.txt
+#go run /home/user/Work_TS/SnapshotWork/snapshotwork_code/copySnapshotFromPeer.go
+#peer channel getinfo -c mychannel
+#peer channel getinfo -c mychannel| grep '{"height":' >> ${PWD}/../archive/filead120.txt
+#peer channel getinfo -c mychannel | cut -d':' -f3 |cut -d',' -f1 >> ${PWD}/../archive/snapheight.txt
+typeset -i curval=$(peer channel getinfo -c mychannel | cut -d':' -f3 |cut -d',' -f1)
 
-chmod 777 -R ${PWD}/archive/
+typeset -i snapval=$(cat ${PWD}/../archive/snapheight.txt)
+#$thsval=10
+
+difval=$(($curval - $snapval))
+echo "$difval"
+# if ($difval>3)
+if [ "$difval" -gt "$thsval" ];
+then
+{
+
+peer snapshot submitrequest -c mychannel -b 0 --peerAddress localhost:7051 --tlsRootCertFile ${PWD}/../organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+docker cp peer0.org1.example.com:/var/hyperledger/production/snapshots/completed/mychannel/ ${PWD}/../archive/$d
+chmod 777 -R ${PWD}/../archive/
+echo "$curval" > ${PWD}/../archive/snapheight.txt
+
+}
+fi
+docker exec peer0.org1.example.com rm -rf /var/hyperledger/production/snapshots/completed/mychannel/5008
+#docker exec peer0.org1.example.com rm -rf /var/hyperledger/production/snapshots/completed/mychannel/*.*
+
